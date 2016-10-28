298524fa016f892c5735c15981300fe83cabbad1
user <user@2-5>
2016-10-28 10:17:00 +0800

    first

---
+++ WebApplication1/WebApplication1/Controllers/memberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class memberController : Controller
+    {
+        // GET: member
+        public ActionResult memberIndex()
+        {
+            return View();
+        }
+    }
+}
